952018ee5738a55305bf8eb947e3335676f9a8c4
Skye Baldock <skye.baldock@gmail.com>
2023-03-31 20:55:09 +1100

    updated gh-pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/skyebaldocksites" element={<Layout />}>
+        <Route path="/skyebaldocksites/" element={<Layout />}>
           <Route index element={<AboutPage />} />
           <Route path="about" element={<AboutPage />} />
           <Route path="services" element={<Services />} />
