3eed9842068c372c703f10005d21a43a2383cd14
Skye <skye.baldock@gmail.com>
2024-01-02 17:52:05 +1000

    entusiasm

--- src/content/AboutText.jsx
+++ src/content/AboutText.jsx
@@ -3,7 +3,7 @@ const AboutText = () => {
   return (
     <>
       <p>
-        Skye has recently completed her Bachelor of Computer Science with Distinction at the University of New South Wales. She is entusiastic and always looking to the next challenge. She has experience in web development and is skilled in data analysis. 
+        Skye has recently completed her Bachelor of Computer Science with Distinction at the University of New South Wales. She is enthusiastic and always looking to the next challenge. She has experience in web development and is skilled in data analysis. 
       </p>
       <p>
         In her spare time, Skye enjoys reading, learning everything she can, hiking and photography. Skye is passionate about travel, and has been to four of the seven continents.
