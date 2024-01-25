3f730fa589002f7e629a5e64b9861143fb704792
Skye <skye.baldock@gmail.com>
2024-01-25 17:08:07 +1100

    aaaaa

--- src/pages/Tableau.jsx
+++ src/pages/Tableau.jsx
@@ -5,9 +5,9 @@ const Tableau = () => {
   return (
     <>
       <p>
-        The embedded pdf displays a dashboard I created with Tableau Public from data taken from <a href='http://insideairbnb.com/get-the-data/'>here</a>. I imported and related data, performed calculations on it, and created a few graphs and put them together to inform a view of the Airbnb market in Tasmania. 
+        The pdf below displays a dashboard I created with Tableau Public from data taken from <a href='http://insideairbnb.com/get-the-data/'>here</a>. I imported and related data, performed calculations on it, and created a few graphs and put them together to inform a view of the Airbnb market in Tasmania. 
       </p>
-      <PdfViewer path="../content/Tasmania Airbnb Dashboard.pdf" />
+      <PdfViewer path="../content/tasmania-airbnb-dashboard.pdf" />
     </>
   );
 }
