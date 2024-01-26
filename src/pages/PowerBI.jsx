9c0d84a88e6b9ee8eef6aef7d0c4c3f7a6342fab
Skye <skye.baldock@gmail.com>
2024-01-26 11:44:24 +1100

    require

--- src/pages/PowerBI.jsx
+++ src/pages/PowerBI.jsx
@@ -7,7 +7,7 @@ const PowerBI = () => {
       <p>
         The below shows a screenshot of a dashboard I created from data taken from <a href='https://github.com/AlexTheAnalyst/Power-BI/blob/main/Power%20BI%20-%20Final%20Project.xlsx'>here</a>. I cleaned the data in Power BI, wrote Python scripts for efficiency, and created this dashboard. It works much better on Power BI, but due to the fact that I do not have a business I had to export it to PDF to share it. It was very interesting data to me, personally, and I drew many unexpected conclusions whilst creating this dashboard.
       </p>
-      <Image src='./content/data-professionals-survey-dashboard.jpg' fluid />
+      <Image src={ require=('../content/data-professionals-survey-dashboard.jpg') } fluid />
     </>
   );
 }
--- src/pages/Tableau.jsx
+++ src/pages/Tableau.jsx
@@ -6,7 +6,7 @@ const Tableau = () => {
       <p>
         The pdf below displays a dashboard I created with Tableau Public from data taken from <a href='http://insideairbnb.com/get-the-data/'>here</a>. I imported and related data, performed calculations on it, and created a few graphs and put them together to inform a view of the Airbnb market in Tasmania. 
       </p>
-      <Image src="../content/tasmania-airbnb-dashboard.jpg"  fluid />
+      <Image src={ require("../content/tasmania-airbnb-dashboard.jpg") }  fluid />
     </>
   );
 }
