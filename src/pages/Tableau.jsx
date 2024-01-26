31412d4bd5ad17b8c411e89a9b34badee6de1f84
Skye <skye.baldock@gmail.com>
2024-01-26 12:08:50 +1100

    may have actually fixed

--- src/pages/Tableau.jsx
+++ src/pages/Tableau.jsx
@@ -1,4 +1,3 @@
-import Image from 'react-bootstrap/Image';
 
 const Tableau = () => {
   return (
@@ -6,7 +5,12 @@ const Tableau = () => {
       <p>
         The pdf below displays a dashboard I created with Tableau Public from data taken from <a href='http://insideairbnb.com/get-the-data/'>here</a>. I imported and related data, performed calculations on it, and created a few graphs and put them together to inform a view of the Airbnb market in Tasmania. 
       </p>
-      <Image src={ require("../content/tasmania-airbnb-dashboard.jpg") }  fluid />
+      <iframe
+        src={ require('../content/tasmania-airbnb-dashboard.pdf') }
+        width="100%"
+        height="600"
+        frameborder="0"
+      />
     </>
   );
 }
