813d8ceddcc4153202369c00e16f4a27a84891c5
Skye <skye.baldock@gmail.com>
2024-01-29 10:35:20 +1100

    pdf viewer

--- src/pages/PowerBI.jsx
+++ src/pages/PowerBI.jsx
@@ -1,3 +1,4 @@
+import { Document, Page } from 'react-pdf';
 
 const PowerBI = () => {
     
@@ -6,12 +7,15 @@ const PowerBI = () => {
       <p>
         The below shows a screenshot of a dashboard I created from data taken from <a href='https://github.com/AlexTheAnalyst/Power-BI/blob/main/Power%20BI%20-%20Final%20Project.xlsx'>here</a>. I cleaned the data in Power BI, wrote Python scripts for efficiency, and created this dashboard. It works much better on Power BI, but due to the fact that I do not have a business I had to export it to PDF to share it. It was very interesting data to me, personally, and I drew many unexpected conclusions whilst creating this dashboard.
       </p>
-      <iframe
-        src={ require('../content/data-professionals-survey-dashboard.pdf') }
-        width="100%"
-        height="600"
-        frameborder="0"
-      />
+      <Document
+        file={ require('../content/data-professionals-survey-dashboard.pdf') }
+      >
+        <Page
+          pageNumber={1}
+          width="100%"
+          height="600"
+        />
+      </Document>
     </>
   );
 }
--- src/pages/Tableau.jsx
+++ src/pages/Tableau.jsx
@@ -1,3 +1,4 @@
+import { Document, Page } from 'react-pdf';
 
 const Tableau = () => {
   return (
@@ -5,12 +6,15 @@ const Tableau = () => {
       <p>
         The pdf below displays a dashboard I created with Tableau Public from data taken from <a href='http://insideairbnb.com/get-the-data/'>here</a>. I imported and related data, performed calculations on it, and created a few graphs and put them together to inform a view of the Airbnb market in Tasmania. 
       </p>
-      <iframe
-        src={ require('../content/tasmania-airbnb-dashboard.pdf') }
-        width="100%"
-        height="600"
-        frameborder="0"
-      />
+      <Document
+        file={ require('../content/tasmania-airbnb-dashboard.pdf') }
+      >
+        <Page
+          pageNumber={1}
+          width="100%"
+          height="600"
+        />
+      </Document>
     </>
   );
 }
